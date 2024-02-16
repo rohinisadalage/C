f6b18f97c70191f8ed8d5e075cc0dc2d68abd457
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-02-16 07:48:21 +0530

    Add files via upload

---
+++ odd_even_if.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter any number:");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+	    printf("The no is Even....");
+    }
+
+	else
+	{
+		printf("The no is Odd....");
+	}
+	return 0;
+}
