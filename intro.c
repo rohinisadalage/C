b66a7d00d849e4dc552942bdd9e57fb174ba0d22
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-02-22 22:45:56 +0530

    Create intro.c

---
+++ intro.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,sum;
+printf("Hello Sanika.......!");
+printf("What is the addition of first and second semester marks?");
+printf("Enter the fisrt semester marks:");
+scanf("%d\n"&num1);
+printf("Enter the second semester marks:");
+scanf("%d\n"&num2);
+sum=num1+num2;
+printf("My first and second semester marks total is:",sum);
+return 0;
+}
