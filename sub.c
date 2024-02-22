cd5096d7008f0f15b2c537af0f5a153ef7b891ee
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-02-22 22:51:13 +0530

    Create sub.c

---
+++ sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int num1,num2,sub;
+printf("Enter value of num1:")
+  scanf("%d\n",&num1;);
+printf("Enter value of num2:")
+  scanf("%d\n",&num1;);
+sub=num1-num2;
+printf("substraction is",sub);
+return 0;
+}
