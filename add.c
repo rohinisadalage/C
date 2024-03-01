eab32413d3726384b7b2ba9536423eb46ac3dc58
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-03-01 08:10:41 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the value for a and b:");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("%d\n",c);
+	c=a-b;
+	printf("%d\n",c);
+	c=a*b;
+	printf("%d\n",c);
+	c=a/b;
+	printf("%d\n",c);
+	return 0;
+}
