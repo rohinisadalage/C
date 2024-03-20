f72fef864c18a4d9ad1b652ed0ddd32d3dea317d
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-03-20 21:58:55 +0530

    ascending.c

---
+++ ascending.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[] = {5, 2, 7, 1, 9}; 
+    int n = sizeof(numbers) / sizeof(numbers[0]);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                int temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+    printf("Numbers in ascending order: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
