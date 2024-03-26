82ee0e05cbe0d4936f4c8ee8c50e84c82ff7ae59
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-03-26 20:48:31 +0530

    Add files via upload

---
+++ ascii_value_for_user_choice.c
@@ -0,0 +1,11 @@
+//WAP to print the user having specific character ASCII value 
+#include<stdio.h>
+int main()
+{
+	char c;
+    printf("Enter the character:\n");
+    scanf("%c",&c);
+    printf("ASCII value of %c is %d\n",c,c);
+	printf("Thank You");    
+	return 0;
+}
