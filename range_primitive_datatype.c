5ac3839faa1699f6df7c5d96575bbe8af90fc1dc
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-07 19:11:42 +0530

    Add files via upload

---
+++ range_primitive_datatype.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+	printf("\n Memory range of the primitive datatype:");
+	printf("\n Integer Memory range %d to %d",INT_MIN,INT_MAX);
+	return 0;
+}
