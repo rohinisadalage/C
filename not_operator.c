43ef696210875f583e08e5b6e8a78ffd4df56dcd
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-15 19:49:56 +0530

    Create not_operator.c

---
+++ not_operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num = 10;
+    int result;
+    result = !num;
+    printf("Original value of num: %d\n", num);
+    printf("Value of num after applying NOT operator: %d\n", result);
+    return 0;
+}
