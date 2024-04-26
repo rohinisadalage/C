509190c72ec5f2cd6e2833a62459542025c5fa83
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-26 20:33:22 +0530

    Create boolean_keyword.c

---
+++ boolean_keyword.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool isTrue = true;
+    bool isFalse = false;
+    printf("isTrue: %d\n", isTrue);
+    printf("isFalse: %d\n", isFalse);
+
+    return 0;
+}
