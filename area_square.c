1213d6368bec8745247256a6eb82e10d06c2dc42
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-05-02 11:32:09 +0530

    Add files via upload

---
+++ area_square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int area,l,b;
+	printf("Enter the value of l:");
+	scanf("%d",&l);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	area=l*b;
+	printf("area of square is%d",area);
+	return 0;
+}
