9e6d37365969eca2f95ae0fe275bad77f3ed56a2
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-07-05 21:50:31 +0530

    Create rectangle_and_symbol.c

---
+++ rectangle_and_symbol.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void printRectangle(int width, int height) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("&");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int width, height;
+
+    // Get the dimensions of the rectangle from the user
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", &width);
+    printf("Enter the height of the rectangle: ");
+    scanf("%d", &height);
+
+    // Print the rectangle
+    printRectangle(width, height);
+
+    return 0;
+}
